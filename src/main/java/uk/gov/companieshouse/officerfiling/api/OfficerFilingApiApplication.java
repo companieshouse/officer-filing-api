80e565cfdfebc245a35a1f0531704a8a442870ea
Simon Boyd <sboyd@companieshouse.gov.uk>
2022-09-01 11:02:37 +0100

    Add starter files from Spring Initializr with additions
    - Spring-generated sources and unit tests
    - pom.xml plus testing and other dependencies
    - updated .gitignore
    
    Resolves:
    DACT-54

---
+++ src/main/java/uk/gov/companieshouse/officerfiling/api/OfficerFilingApiApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.officerfiling.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficerFilingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficerFilingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gov/companieshouse/officerfiling/api/OfficerFilingApiApplicationTests.java
@@ -0,0 +1,19 @@
+package uk.gov.companieshouse.officerfiling.api;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyString;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficerFilingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+        assertThat("TODO", is(not(emptyString())));
+	}
+
+}
