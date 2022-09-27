030609358249ce206f4d38711888a79c155c4f07
Simon Boyd <sboyd@companieshouse.gov.uk>
2022-09-27 10:51:03 +0100

    Fix integration test errors
    Resolves:
    DACT-134

--- src/test/java/uk/gov/companieshouse/officerfiling/api/controller/OfficerFilingControllerImplIT.java
+++ src/test/java/uk/gov/companieshouse/officerfiling/api/controller/OfficerFilingControllerImplIT.java
@@ -1,13 +1,11 @@
 package uk.gov.companieshouse.officerfiling.api.controller;
 
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.time.Instant;
 import java.time.LocalDate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -16,10 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import uk.gov.companieshouse.api.model.transaction.Transaction;
-import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.officerfiling.api.model.dto.OfficerFilingDto;
-import uk.gov.companieshouse.officerfiling.api.model.entity.OfficerFiling;
 import uk.gov.companieshouse.officerfiling.api.model.mapper.OfficerFilingMapper;
 import uk.gov.companieshouse.officerfiling.api.service.OfficerFilingService;
 
@@ -45,7 +40,12 @@ class OfficerFilingControllerImplIT {
 
     @Test
     void createFilingWhenTM01PayloadOKThenResponse201() throws Exception {
-        String request = "{" + TM01_FRAGMENT + "}";
+        final var body = "{" + TM01_FRAGMENT + "}";
+        final var dto = OfficerFilingDto.builder()
+                .referenceEtag("etag")
+                .referenceOfficerId("id")
+                .resignedOn(LocalDate.of(2022, 9, 13))
+                .build();
 
         mockMvc.perform(post("/transactions/{id}/officers", TRANS_ID).content(body)
                         .contentType("application/json"))
