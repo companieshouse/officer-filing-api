f8b8a8dfe76f25eb437e535dffd2b4421321c1a0
PhilipGlover <pglover@companieshouse.gov.uk>
2022-10-14 07:54:19 +0100

    add snake_case strategy to data passed to Chips consumer

--- src/main/java/uk/gov/companieshouse/officerfiling/api/utils/MapHelper.java
+++ src/main/java/uk/gov/companieshouse/officerfiling/api/utils/MapHelper.java
@@ -2,6 +2,7 @@ package uk.gov.companieshouse.officerfiling.api.utils;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import java.util.Map;
 
 public final class MapHelper {
@@ -13,7 +14,7 @@ public final class MapHelper {
 
     public static Map<String, Object> convertObject(Object obj) {
         if (mapper == null) {
-            mapper = new ObjectMapper();
+            mapper = new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
         }
 
         return mapper.convertValue(obj, new TypeReference<Map<String, Object>>() {
