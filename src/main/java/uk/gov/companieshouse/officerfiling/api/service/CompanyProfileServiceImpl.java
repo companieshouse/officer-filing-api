d3ef078cb354e0f06872935aac13d18ce8a7b783
jditchburn <jditchburn@companieshouse.gov.uk>
2023-01-06 15:40:19 +0000

    DACT-269 Add company profile infrastructure and validate termination date against incorporation

---
+++ src/main/java/uk/gov/companieshouse/officerfiling/api/service/CompanyProfileServiceImpl.java
@@ -0,0 +1,52 @@
+package uk.gov.companieshouse.officerfiling.api.service;
+
+import org.springframework.stereotype.Service;
+import uk.gov.companieshouse.api.handler.exception.URIValidationException;
+import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.officerfiling.api.exception.CompanyProfileServiceException;
+import uk.gov.companieshouse.officerfiling.api.utils.LogHelper;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CompanyProfileServiceImpl implements CompanyProfileService {
+
+    private final ApiClientService apiClientService;
+    private final Logger logger;
+
+    public CompanyProfileServiceImpl(ApiClientService apiClientService, Logger logger) {
+        this.apiClientService = apiClientService;
+        this.logger = logger;
+    }
+
+    /**
+     * Query the company profile service for a given transaction.
+     *
+     * @param transactionId the ID of the related transaction
+     * @param companyNumber the Company Number
+     * @param ericPassThroughHeader includes authorisation details
+     * @return the company profile if found
+     * @throws CompanyProfileServiceException if not found or an error occurred
+     */
+    @Override
+    public CompanyProfileApi getCompanyProfile(final String transactionId, final String companyNumber, final String ericPassThroughHeader)
+            throws CompanyProfileServiceException {
+        try {
+            final String uri = "/company/" + companyNumber;
+            final CompanyProfileApi companyProfile = apiClientService.getInternalOauthAuthenticatedClient(ericPassThroughHeader)
+                            .company()
+                            .get(uri)
+                            .execute()
+                            .getData();
+            final Map<String, Object> logMap = LogHelper.createLogMap(transactionId);
+            logMap.put("company_number", companyNumber);
+            logger.debugContext(companyNumber, "Retrieved company profile details", logMap);
+            return companyProfile;
+        }
+        catch (final URIValidationException | IOException e) {
+            throw new CompanyProfileServiceException("Error Retrieving company profile " + companyNumber, e);
+        }
+    }
+}
