881f01a219c0f18133c62db1339cf8188474ca99
KALatchman <klatchman@companieshouse.gov.uk>
2023-02-16 15:50:41 +0000

    Logging interceptor created

---
+++ src/main/java/uk/gov/companieshouse/officerfiling/api/interceptor/RequestLoggingInterceptor.java
@@ -0,0 +1,32 @@
+package uk.gov.companieshouse.officerfiling.api.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+import uk.gov.companieshouse.logging.util.RequestLogger;
+import uk.gov.companieshouse.officerfiling.api.OfficerFilingApiApplication;
+
+public class RequestLoggingInterceptor implements HandlerInterceptor, RequestLogger {
+
+    private Logger logger;
+
+    @Autowired
+    public RequestLoggingInterceptor() {
+        logger = LoggerFactory.getLogger(OfficerFilingApiApplication.APP_NAMESPACE);
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        logStartRequestProcessing(request, logger);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+        logEndRequestProcessing(request, response, logger);
+    }
+}
