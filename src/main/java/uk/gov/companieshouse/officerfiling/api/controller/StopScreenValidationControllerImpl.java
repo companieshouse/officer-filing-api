1ed529833d10e9cfe3158e253d41317c0a12ec63
anona-w <117649383+anona-w@users.noreply.github.com>
2023-04-28 16:20:35 +0100

    Added new route and fixed dissolved check

--- src/main/java/uk/gov/companieshouse/officerfiling/api/controller/StopScreenValidationControllerImpl.java
+++ src/main/java/uk/gov/companieshouse/officerfiling/api/controller/StopScreenValidationControllerImpl.java
@@ -3,14 +3,17 @@ package uk.gov.companieshouse.officerfiling.api.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.officerfiling.api.service.CompanyProfileService;
 import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
 
+@RestController
 public class StopScreenValidationControllerImpl implements StopScreenValidationController {
 
     private final Logger logger;
@@ -24,9 +27,9 @@ public class StopScreenValidationControllerImpl implements StopScreenValidationC
 
     @Override
     @ResponseBody
-    @GetMapping(value = "/officer-filing/company/{companyNumber}/past-future-dissolved", produces = {"application/json"})
+    @GetMapping(value = "/officer-filing/company/{companyNumber}/stop-screen-checks/past-future-dissolved", produces = {"application/json"})
     public ResponseEntity<Object> getCurrentOrFutureDissolved(
-            @RequestAttribute("companyNumber") String companyNumber,
+            @PathVariable("companyNumber") String companyNumber,
             final HttpServletRequest request) {
 
         final var passthroughHeader =
@@ -36,7 +39,7 @@ public class StopScreenValidationControllerImpl implements StopScreenValidationC
 
         final var companyProfile = companyProfileService.getCompanyProfile(transactionId, companyNumber, passthroughHeader);
 
-        if (companyProfile.getDateOfCessation() != null || companyProfile.getCompanyStatus() == "dissolved") {
+        if (companyProfile.getDateOfCessation() != null || Objects.equals(companyProfile.getCompanyStatus(), "dissolved")){
             return ResponseEntity.status(HttpStatus.OK).body(true);
         } else {
             return ResponseEntity.status(HttpStatus.OK).body(false);
