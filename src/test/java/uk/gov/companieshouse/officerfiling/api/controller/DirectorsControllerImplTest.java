428f919c3e23a79e1d919a1d0ca8423b192d86c3
jbestwickch <jbestwick@companieshouse.gov.uk>
2023-06-02 11:07:14 +0100

    Update unit tests to check for server error result from public API

--- src/test/java/uk/gov/companieshouse/officerfiling/api/controller/DirectorsControllerImplTest.java
+++ src/test/java/uk/gov/companieshouse/officerfiling/api/controller/DirectorsControllerImplTest.java
@@ -95,6 +95,17 @@ class DirectorsControllerImplTest {
   }
 
   @Test
+  void getListOfActiveDirectorsDetailsThrowsExceptionWhenServerError() throws OfficerServiceException {
+    when(request.getHeader(ApiSdkManager.getEricPassthroughTokenHeader())).thenReturn(PASSTHROUGH_HEADER);
+    when(officerService.getListOfActiveDirectorsDetails(request, TRANS_ID, COMPANY_NUMBER, PASSTHROUGH_HEADER))
+            .thenThrow(serviceException);
+    when(serviceException.getCause()).thenReturn(serviceException);
+    when(serviceException.getMessage()).thenReturn("Internal error");
+    var response = testService.getListActiveDirectorsDetails(transaction, request);
+    assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+  }
+
+  @Test
   void getRemoveCheckAnswersDirectorDetailsWhenFound() throws Exception {
     when(request.getHeader(ApiSdkManager.getEricPassthroughTokenHeader())).thenReturn(PASSTHROUGH_HEADER);
     when(officerFilingService.get(SUBMISSION_ID, TRANS_ID)).thenReturn(officerFilingOptional);
