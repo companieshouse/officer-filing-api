d19a0a058a0c4825a945fcb50d89a93621d32032
rwalrondch <rwalrond@companieshouse.gov.uk>
2023-07-24 09:27:43 +0100

    DACT-531 - test change for code from DACT-417

--- src/test/java/uk/gov/companieshouse/officerfiling/api/service/FilingDataServiceImplTest.java
+++ src/test/java/uk/gov/companieshouse/officerfiling/api/service/FilingDataServiceImplTest.java
@@ -43,6 +43,7 @@ class FilingDataServiceImplTest {
     private static final Instant RESIGNED_ON_INS = Instant.parse("2022-10-05T00:00:00Z");
     public static final String FIRSTNAME = "JOE";
     public static final String LASTNAME = "BLOGGS";
+    public static final String COMPANY_NAME = "Company Name";
     public static final String FULL_NAME = FIRSTNAME + " " + LASTNAME;
     public static final String DATE_OF_BIRTH_STR = "2000-10-20";
     private static final String PASSTHROUGH_HEADER = "passthrough";
@@ -124,11 +125,10 @@ class FilingDataServiceImplTest {
 
     @Test
     void generateCorporateOfficerFilingWhenFound() {
-        final var filingData = new FilingData(FIRSTNAME, LASTNAME, null, RESIGNED_ON_STR, true);
+        final var filingData = new FilingData(null, COMPANY_NAME, null, RESIGNED_ON_STR, true);
         final var data = OfficerFilingData.builder()
                 .referenceAppointmentId(REF_APPOINTMENT_ID)
-                .firstName(FIRSTNAME)
-                .lastName(LASTNAME)
+                .name(COMPANY_NAME)
                 .resignedOn(RESIGNED_ON_INS)
                 .build();
         final var officerFiling = OfficerFiling.builder()
@@ -136,8 +136,8 @@ class FilingDataServiceImplTest {
                 .build();
 
         when(companyAppointment.getOfficerRole()).thenReturn("corporate-director");
-        when(companyAppointment.getForename()).thenReturn(FIRSTNAME);
-        when(companyAppointment.getSurname()).thenReturn(LASTNAME);
+        when(companyAppointment.getSurname()).thenReturn(null);
+        when(companyAppointment.getName()).thenReturn(COMPANY_NAME);
         when(officerFilingService.get(FILING_ID, TRANS_ID)).thenReturn(Optional.of(officerFiling));
         when(filingAPIMapper.map(officerFiling)).thenReturn(filingData);
         when(transactionService.getTransaction(TRANS_ID, PASSTHROUGH_HEADER)).thenReturn(transaction);
@@ -148,14 +148,14 @@ class FilingDataServiceImplTest {
         final var filingApi = testService.generateOfficerFiling(TRANS_ID, FILING_ID, PASSTHROUGH_HEADER);
 
         final Map<String, Object> expectedMap =
-                Map.of("first_name", FIRSTNAME, "last_name", LASTNAME,
+                Map.of("last_name", COMPANY_NAME,
                         "resigned_on", RESIGNED_ON_STR,
                         "is_corporate_director", true);
 
         assertThat(filingApi.getData(), is(equalTo(expectedMap)));
         assertThat(filingApi.getKind(), is("officer-filing#termination"));
         assertThat(filingApi.getDescription(), is("(TM01) Termination of appointment of director. Terminating appointment of "
-                + FIRSTNAME + " " + LASTNAME.toUpperCase()  + " on 16 March 2023"));
+                + COMPANY_NAME  + " on 16 March 2023"));
     }
 
     @Test
