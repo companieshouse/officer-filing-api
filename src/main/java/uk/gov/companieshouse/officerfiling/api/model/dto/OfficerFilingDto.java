9460a06083fc4c04e6ddd1a758a8ee7ab71b40b8
Milan Desai <mdesai@companieshouse.gov.uk>
2023-11-10 09:09:20 +0000

    DACT-1026 place-holder POC changes for using custom Deserializer

--- src/main/java/uk/gov/companieshouse/officerfiling/api/model/dto/OfficerFilingDto.java
+++ src/main/java/uk/gov/companieshouse/officerfiling/api/model/dto/OfficerFilingDto.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import java.util.StringJoiner;
 import java.util.function.Consumer;
 import org.springframework.validation.annotation.Validated;
+import uk.gov.companieshouse.officerfiling.api.config.JsonBooleanDeserializer;
 
 @JsonDeserialize(builder = OfficerFilingDto.Builder.class)
 @Validated
@@ -484,6 +485,7 @@ public class OfficerFilingDto {
             return this;
         }
 
+        @JsonDeserialize(using = JsonBooleanDeserializer.class)
         public Builder consentToAct(final Boolean value) {
             buildSteps.add(data -> data.consentToAct = value);
             return this;
