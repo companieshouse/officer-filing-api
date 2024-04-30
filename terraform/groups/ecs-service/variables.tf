7fb7aee3a0d9e8367d36927e24bd27f2345ae653
gjackson-ch <gjackson@companieshouse.gov.uk>
2024-04-30 14:36:24 +0100

    Removing duplicate log levels

--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -129,12 +129,6 @@ variable "officer_filing_api_version" {
   description = "The version of the officer-filing-api container to run."
 }
 
-variable "log_level" {
-  default     = "info"
-  type        = string
-  description = "The log level for services to use: trace, debug, info or error"
-}
-
 variable "eric_version" {
   type        = string
   description = "The version of the eric container to run."
