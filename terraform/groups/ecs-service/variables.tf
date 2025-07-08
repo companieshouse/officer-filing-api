b845cac33ab086476db704daf61b16018e7b4a0d
gjackson-ch <gjackson@companieshouse.gov.uk>
2025-07-08 16:57:26 +0100

    Tech debt changes

--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -130,12 +130,6 @@ variable "eric_version" {
   description = "The version of the eric container to run."
 }
 
-variable "log_level" {
-  default     = "info"
-  type        = string
-  description = "The log level for services to use: trace, debug, info or error"
-}
-
 variable "officer_filing_api_version" {
   type        = string
   description = "The version of the officer-filing-api container to run."
